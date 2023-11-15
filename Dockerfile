cc779c635062c220c53e85624130029c9a597616
elad <elad@tokagroup.com>
2023-11-15 09:58:19 +0200

    some cleanup and fixes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM ubuntu:18.04
 
-LABEL maintainer="tomer.klein@gmail.com"
+ENV PYTHONIOENCODING=utf-8
+ENV LANG=C.UTF-8
 
 #install pip3
 RUN apt update
@@ -8,49 +9,14 @@ RUN apt update
 RUN apt install -yqq python3-pip
 
 #install python paho-mqtt client and urllib3
-RUN pip3 install --upgrade pip setuptools  --no-cache-dir && \
-    pip3 install paho-mqtt --no-cache-dir && \
-    pip3 install urllib3 --no-cache-dir && \
-    pip3 install loguru --no-cache-dir && \
-    pip3 install apprise --no-cache-dir
+COPY requirements.txt requirements.txt
+RUN pip3 install --upgrade pip setuptools --no-cache-dir && \
+    pip3 install -r requirements.txt --no-cache-dir
 
-
-ENV PYTHONIOENCODING=utf-8
-
-ENV LANG=C.UTF-8
-
-#Mqtt broker address (ip or fqdn)
-ENV MQTT_HOST "127.0.0.1"
-
-#Mqtt broker port - 1883 is the common
-ENV MQTT_PORT "1883"
-
-#Mqtt broker username
-ENV MQTT_USER "user"
-
-#Mqtt broker password
-ENV MQTT_PASS "password"
-
-#Debug Mode for testing
-ENV DEBUG_MODE "False"
-
-ENV REGION "*"
-
-ENV NOTIFIERS ""
-
-ENV INCLUDE_TEST_ALERTS "False"
-
-ENV MQTT_TOPIC "/redalert"
 
 #Create working directory
 RUN mkdir /opt/redalert
-
 COPY redalert.py /opt/redalert
-
-#Cleanup
-#RUN apt remove python3-pip --yes && \
-#    apt autoremove --yes && \
-#    rm -rf /var/cache/*
 
 
 ENTRYPOINT ["/usr/bin/python3", "/opt/redalert/redalert.py"]
--- redalert.py
+++ redalert.py
@@ -1,140 +1,153 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import threading
 import paho.mqtt.client as mqtt
-import urllib3
+import requests
 import os
 from loguru import logger
 import time
-import codecs
 import apprise
 import json
 
 os.environ['PYTHONIOENCODING'] = 'utf-8'
 os.environ['LANG'] = 'C.UTF-8'
 
-#mqtt connection Params
-server = os.getenv('MQTT_HOST')
-#Default port is 1883
-port = int(os.getenv('MQTT_PORT'))
-user = os.getenv('MQTT_USER')
-passw = os.getenv('MQTT_PASS')
-debug = os.getenv('DEBUG_MODE')
-region = os.getenv('REGION')
-NOTIFIERS = os.getenv("NOTIFIERS")
-MQTT_TOPIC = os.environ.get("MQTT_TOPIC", "/redalert")
-INCLUDE_TEST_ALERTS = os.getenv("INCLUDE_TEST_ALERTS")
+# mqtt connection Params
+MQTT_HOST = os.getenv('MQTT_HOST', "127.0.0.1")
+MQTT_PORT = int(os.getenv('MQTT_PORT', 1883))  # Default port is 1883
+MQTT_USER = os.getenv("MQTT_USER", "user")
+MQTT_PASS = os.getenv("MQTT_PASS", "password")
+MQTT_TOPIC = os.environ.get("MQTT_TOPIC", "redalert")
+ALERTS_TOPIC = f"{MQTT_TOPIC}/alerts"
+DATA_TOPIC = f"{MQTT_TOPIC}/data"
+STATUS_TOPIC = f"{MQTT_TOPIC}/status"
 
-# reader = codecs.getreader('utf-8')
+DEBUG_MODE = os.getenv("DEBUG_MODE", 'False').lower() in ('true', '1', 't')
+DEBUG_URL = os.getenv("DEBUG_URL", "http://localhost/alerts.json")
 
-logger.info("Monitoring alerts for :" + region)
+ALERTS_REGION = os.getenv("REGION", "*")
+NOTIFIERS = os.getenv("NOTIFIERS", "")
+INCLUDE_TEST_ALERTS = os.getenv("INCLUDE_TEST_ALERTS", 'False').lower() in ('true', '1', 't')
 
 
-#Setting Request Headers
-http = urllib3.PoolManager()
-_headers = {'Referer':'https://www.oref.org.il/','User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",'X-Requested-With':'XMLHttpRequest'}
-url= 'https://www.oref.org.il/WarningMessages/alert/alerts.json'
-if debug == 'True':
-   url = 'http://localhost/alerts.json'
+logger.info(f"Monitoring alerts for: {ALERTS_REGION}")
 
-#Check Connection Status
+# Setting Request Headers
+GET_ALERT_HEADERS = {'Referer': 'https://www.oref.org.il/',
+                     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
+                     'X-Requested-With': 'XMLHttpRequest'}
+ALERTS_URL = 'https://www.oref.org.il/WarningMessages/alert/alerts.json'
+if DEBUG_MODE:
+    ALERTS_URL = DEBUG_URL
+
+SENT_ALERTS_IDS = {0}
+
+
+def main():
+    # Setting apprise Job Manager
+    apprise_jobs = apprise.Apprise()
+
+    # Setting up MqttClient
+    mqtt_client = mqtt.Client("redalert")
+    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_disconnect = on_disconnect
+    mqtt_client.on_log = on_log  # set client logging
+    mqtt_client.loop_start()
+    logger.info("Connecting to broker")
+    mqtt.Client.connected_flag = False  # create flag in class
+    mqtt_client.connect(MQTT_HOST, port=MQTT_PORT, keepalive=3600)
+
+    while not mqtt_client.connected_flag:  # wait in loop
+        logger.info("In wait loop")
+        time.sleep(1)
+    logger.info("in Main Loop")
+    mqtt_client.loop_stop()  # Stop loop
+
+    if len(NOTIFIERS) != 0:
+        logger.info("Setting Apprise Alert")
+        jobs = NOTIFIERS.split()
+        for job in jobs:
+            logger.info("Adding: " + job)
+            apprise_jobs.add(job)
+
+    monitor(mqtt_client, apprise_jobs)
+
+
+# Check Connection Status
 def on_connect(client, userdata, flags, rc):
-    if rc==0:
-        client.connected_flag=True #set flag
-        logger.info("connected OK Returned code=" + str(rc))
+    if rc == 0:
+        client.connected_flag = True  # set flag
+        logger.info(f"connected OK Returned code={rc}")
     else:
-       if rc==1:
-          logger.error("Connection refused – incorrect protocol version")
-       if rc==2:
-           logger.error("Connection refused – invalid client identifier")
-       if rc==3:
-           logger.error("Connection refused – server unavailable")
-       if rc==4:
-           logger.error("Connection refused – bad username or password")
-       if rc==5:
-           logger.error("Connection refused – not authorised")
-        
+        if rc == 1:
+            logger.error("Connection refused – incorrect protocol version")
+        if rc == 2:
+            logger.error("Connection refused – invalid client identifier")
+        if rc == 3:
+            logger.error("Connection refused – server unavailable")
+        if rc == 4:
+            logger.error("Connection refused – bad username or password")
+        if rc == 5:
+            logger.error("Connection refused – not authorised")
+
+
 def on_disconnect(client, userdata, rc):
-    logger.info("disconnecting reason  "  +str(rc))
-    client.connected_flag=False
-    client.disconnect_flag=True
-    client.connect(server)
+    logger.info(f"disconnecting reason: {rc}")
+    client.connected_flag = False
+    client.disconnect_flag = True
+    client.connect(MQTT_HOST)
 
 
 def on_log(client, userdata, level, buf):
     logger.inf(buf)
 
-alerts = [0]
 
-# Setting apprise Job Manager
-apobj = apprise.Apprise()
-
-#Setting up MqttClient
-client = mqtt.Client("redalert")
-client.username_pw_set(user,passw)
-client.on_connect=on_connect
-client.on_disconnect=on_disconnect
-client.on_log=on_log # set client logging
-client.loop_start()
-logger.info("Connecting to broker")
-mqtt.Client.connected_flag=False#create flag in class
-client.connect(server, keepalive=3600)
-
-while not client.connected_flag: #wait in loop
-    logger.info("In wait loop")
-    time.sleep(1)
-logger.info("in Main Loop")
-client.loop_stop()    #Stop loop 
-
-
-if len(NOTIFIERS)!=0:
-    logger.info("Setting Apprise Alert")
-    jobs=NOTIFIERS.split()
-    for job in jobs:
-        logger.info("Adding: " + job)
-        apobj.add(job)
-
-def alarm_on(data):
-    client.publish(MQTT_TOPIC + "/data",str(data["data"]),qos=0,retain=False)
-    client.publish(MQTT_TOPIC,'on',qos=0,retain=False)
-    if len(NOTIFIERS)!=0:
+def alarm_on(mqtt_client: mqtt.Client, apprise_jobs: apprise.Apprise, data: dict):
+    mqtt_client.publish(DATA_TOPIC, json.dumps(data["data"]), qos=0, retain=False)
+    mqtt_client.publish(ALERTS_TOPIC, json.dumps(data), qos=0, retain=False)
+    mqtt_client.publish(STATUS_TOPIC, 'on', qos=0, retain=False)
+    if len(NOTIFIERS) != 0:
         logger.info("Alerting using Notifires")
-        apobj.notify(
-            body='באזורים הבאים: \r\n ' + ', '.join(data["data"]) + '\r\n' + str(data["desc"] ),
+        apprise_jobs.notify(
+            body='באזורים הבאים: \r\n ' + ', '.join(data["data"]) + '\r\n' + str(data["desc"]),
             title=str(data["title"]),
-            )
+        )
 
 
-def alarm_off():
-    client.publish(MQTT_TOPIC + "/alarm",'off',qos=0,retain=False)
-    client.publish(MQTT_TOPIC,"No active alerts",qos=0,retain=False)
+def alarm_off(mqtt_client: mqtt.Client):
+    mqtt_client.publish(STATUS_TOPIC, "No active alerts", qos=0, retain=False)
+
 
 def is_test_alert(alert):
     # if includes, all alerts are treated as not test
-    return INCLUDE_TEST_ALERTS == 'False' and ('בדיקה' in alert['data'] or 'בדיקה מחזורית' in alert['data'])
+    return not INCLUDE_TEST_ALERTS and ('בדיקה' in alert['data'] or 'בדיקה מחזורית' in alert['data'])
 
-def monitor():
-  #start the timer
-  threading.Timer(1, monitor).start()
-  #Check for Alerts
-  r = http.request('GET',url,headers=_headers)
-  r.encoding = 'utf-8'
-  alert_data = r.data.decode('utf-8-sig').strip("/n").strip()
-  #Check if data contains alert data
-  try:
-      if alert_data != '':
-          alert = json.loads(alert_data)
-          if region in alert["data"] or region=="*":
-              if alert["id"] not in alerts and not is_test_alert(alert):
-                  alerts.append(alert["id"])
-                  alarm_on(alert)
-                  logger.info(str(alert))
-      else:
-         alarm_off()
-  except Exception as ex:
-         logger.error(str(ex))
-  finally:
-     r.release_conn()
+
+def monitor(mqtt_client, apprise_jobs):
+    # start the timer
+    threading.Timer(1, monitor, args=([mqtt_client, apprise_jobs])).start()
+
+    try:
+        # Check for Alerts
+        with requests.get(ALERTS_URL, headers=GET_ALERT_HEADERS) as r:
+            r.encoding = 'utf-8-sig'
+            alert_data = r.text
+
+        # Check if data contains alert data
+        alert_data = alert_data.strip()
+
+        if alert_data:
+            alert = json.loads(alert_data)
+            if ALERTS_REGION in alert["data"] or ALERTS_REGION == "*":
+                if (alert["id"] not in SENT_ALERTS_IDS and not is_test_alert(alert)) or DEBUG_MODE:
+                    SENT_ALERTS_IDS.add(alert["id"])
+                    alarm_on(mqtt_client, apprise_jobs, alert)
+                    logger.info(json.dumps(alert))
+        else:
+            alarm_off(mqtt_client)
+    except Exception as ex:
+        logger.critical(f"Exception in monitor: {ex}")
+
 
 if __name__ == '__main__':
-   monitor()
+    main()
